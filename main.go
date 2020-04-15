fb8a25d5be46b8065be7752ea7ecae1d37eb7205
Shivamani Patil <shivamanipatil10@gmail.com>
2020-04-15 13:23:43 +0530

    count flag added

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"github.com/shivamanipatil/Goping/ping"
+	"github.com/shivamanipatil/GoPing/ping"
 )
 
 func main() {
+
+	//Command line args and flags
+	hostPtr := flag.String("host", "localhost", "Host name or IP address")
+	countPtr := flag.Int("c", -1, "Stop after sending this number of requests.")
+	flag.Parse()
+
+	//Go routine to listen for SIGINT signal
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
-	host := os.Args[1]
-	totalReq := 0
-	successfulReq := 0
-
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
 	go func() {
 		sig := <-sigs
 		fmt.Println()
@@ -26,19 +29,24 @@ func main() {
 		done <- true
 	}()
 
-	fmt.Println("Starting")
+	//Ping loop
+	totalReq := 0
+	successfulReq := 0
 Loop:
 	for {
 		select {
 		case <-done:
 			break Loop
-
 		default:
+			if *countPtr == totalReq {
+				break Loop
+			}
+			fmt.Println(*countPtr, totalReq)
 			time.Sleep(time.Second)
-			addr, duration, err := ping.Ping(host)
+			addr, duration, err := ping.Ping(*hostPtr, successfulReq)
 			totalReq++
 			if err != nil {
-				fmt.Print("Not succefull")
+				fmt.Print(err)
 				continue
 			}
 			fmt.Printf("Host is %v and duration is %v\n", addr, duration)
--- ping/ping.go
+++ ping/ping.go
@@ -10,26 +10,30 @@ import (
 	"golang.org/x/net/ipv4"
 )
 
-func Ping(domain string) (*net.IPAddr, time.Duration, error) {
+func Ping(domain string, seqNumber int) (*net.IPAddr, time.Duration, error) {
 	//Resolve IP address
 	addr, err := net.ResolveIPAddr("ip", domain)
 	if err != nil {
 		return nil, 0, err
 	}
 
+	//Connection for listening packets
 	conn, err := net.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
 		return addr, 0, err
 	}
 	defer conn.Close()
 
+	//Making message body
+	data := make([]byte, 10)
+	copy(data[:], "abcd")
 	msg := icmp.Message{
 		Type: ipv4.ICMPTypeEcho,
 		Code: 0,
 		Body: &icmp.Echo{
 			ID:   os.Getpid() & 0xffff,
-			Seq:  1,
-			Data: []byte(""),
+			Seq:  seqNumber,
+			Data: data,
 		}}
 	b, err := msg.Marshal(nil)
 	if err != nil {
@@ -56,6 +60,7 @@ func Ping(domain string) (*net.IPAddr, time.Duration, error) {
 		return addr, 0, err
 	}
 	if parsedMsg.Type == ipv4.ICMPTypeEchoReply {
+		fmt.Printf("Message body of size %v\n %+v\n", n, parsedMsg.Body)
 		return addr, span, nil
 	} else {
 		return addr, 0, fmt.Errorf("Received %+v from %v", parsedMsg, receiveAddr)
