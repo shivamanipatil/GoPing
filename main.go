55da8ab4bc3dc4e99905d0dac375c89664af2d71
Shivamani Patil <shivamanipatil10@gmail.com>
2020-04-16 00:13:31 +0530

    main.go: ipv6 flag is usable

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,12 +14,18 @@ import (
 
 func main() {
 
-	//Command line args and flags
+	//Command line flags
 	hostPtr := flag.String("host", "localhost", "Host name or IP address")
 	countPtr := flag.Int("c", -1, "Stop after sending this number of requests. Default -1 is for infinite.")
 	intervalPtr := flag.Float64("i", 1, "Interval seconds between two requests.")
 	sizePtr := flag.Int("s", 56, "Size of packet.")
+	protocolPtr := flag.Int("p", 4, "Protocol number 4 for ipv4 and 6 for ipv6.")
 	flag.Parse()
+
+	if *protocolPtr != 4 && *protocolPtr != 6 {
+		fmt.Printf("Enter 4 for ipv4 and 6 for ipv6.\n")
+		os.Exit(1)
+	}
 
 	//Go routine to listen for SIGINT signal
 	sigs := make(chan os.Signal, 1)
@@ -31,11 +38,20 @@ func main() {
 		done <- true
 	}()
 
-	//Ping loop
+	//Variable initialization
 	totalReq := 0
 	receivedReq := 0
 	var durations []time.Duration
+	//Generic type to hold ping.Ping4 and ping.Ping6
+	var Ping func(domain string, seqNumber int, size int) (*net.IPAddr, time.Duration, error)
+	if *protocolPtr == 4 {
+		Ping = ping.Ping4
+	} else {
+		Ping = ping.Ping6
+	}
 	fmt.Printf("PING %v sending %v bytes of data\n", *hostPtr, *sizePtr)
+
+	//ping loop
 Loop:
 	for {
 		select {
@@ -46,11 +62,11 @@ Loop:
 				break Loop
 			}
 			time.Sleep(time.Duration(*intervalPtr) * time.Second)
-			_, duration, err := ping.Ping4(*hostPtr, receivedReq, *sizePtr)
+			_, duration, err := Ping(*hostPtr, receivedReq, *sizePtr)
 			totalReq++
 			if err != nil {
 				fmt.Print(err)
-				continue
+				break Loop
 			}
 			fmt.Printf("time=%v\n", duration)
 			durations = append(durations, duration)
