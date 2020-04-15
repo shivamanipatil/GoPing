61806c56452f8e1d5ae81fa8425fff8f3eb7dc41
Shivamani Patil <shivamanipatil10@gmail.com>
2020-04-15 16:31:57 +0530

    ping/ping.go: ipv6 function added.

--- ping/ping.go
+++ ping/ping.go
@@ -8,9 +8,10 @@ import (
 
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 )
 
-func Ping(domain string, seqNumber int) (*net.IPAddr, time.Duration, error) {
+func Ping4(domain string, seqNumber int, size int) (*net.IPAddr, time.Duration, error) {
 	//Resolve IP address
 	addr, err := net.ResolveIPAddr("ip", domain)
 	if err != nil {
@@ -25,7 +26,7 @@ func Ping(domain string, seqNumber int) (*net.IPAddr, time.Duration, error) {
 	defer conn.Close()
 
 	//Making message body
-	data := make([]byte, 10)
+	data := make([]byte, size)
 	copy(data[:], "abcd")
 	msg := icmp.Message{
 		Type: ipv4.ICMPTypeEcho,
@@ -60,6 +61,64 @@ func Ping(domain string, seqNumber int) (*net.IPAddr, time.Duration, error) {
 		return addr, 0, err
 	}
 	if parsedMsg.Type == ipv4.ICMPTypeEchoReply {
+		fmt.Printf("%v bytes from %v: icmp_seq=%v ", n, receiveAddr, parsedMsg.Body.(*icmp.Echo).Seq)
+		return addr, span, nil
+	} else {
+		return addr, 0, fmt.Errorf("Received %+v from %v", parsedMsg, receiveAddr)
+	}
+
+}
+
+func Ping6(domain string, seqNumber int) (*net.IPAddr, time.Duration, error) {
+	//Resolve IP address
+	addr, err := net.ResolveIPAddr("ip", domain)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	//Connection for listening packets
+	conn, err := net.ListenPacket("ip6:58", "::")
+	if err != nil {
+		return addr, 0, err
+	}
+	defer conn.Close()
+
+	//Making message body
+	data := make([]byte, 10)
+	copy(data[:], "abcd")
+	msg := icmp.Message{
+		Type: ipv6.ICMPTypeEchoRequest,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID:   os.Getpid() & 0xffff,
+			Seq:  seqNumber,
+			Data: data,
+		}}
+	b, err := msg.Marshal(nil)
+	if err != nil {
+		return addr, 0, err
+	}
+
+	start := time.Now()
+	i, err := conn.WriteTo(b, addr)
+	if err != nil {
+		return addr, 0, err
+	}
+	if i != len(b) {
+		return addr, 0, fmt.Errorf("Couldn't write whole message...")
+	}
+
+	receiveBuffer := make([]byte, 2000)
+	n, receiveAddr, err := conn.ReadFrom(receiveBuffer)
+	if err != nil {
+		return addr, 0, err
+	}
+	span := time.Since(start)
+	parsedMsg, err := icmp.ParseMessage(1, receiveBuffer[:n])
+	if err != nil {
+		return addr, 0, err
+	}
+	if parsedMsg.Type == ipv6.ICMPTypeEchoReply {
 		fmt.Printf("Message body of size %v\n %+v\n", n, parsedMsg.Body)
 		return addr, span, nil
 	} else {
